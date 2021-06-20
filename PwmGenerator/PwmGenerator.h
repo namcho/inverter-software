08649de07bdccb047ef1b330c067ae30b2dc0635
namcho <nazimyildiz90@gmail.com>
2021-06-20 16:41:44 +0300

    Pwm generation attributes are added.

--- PwmGenerator/PwmGenerator.h
+++ PwmGenerator/PwmGenerator.h
@@ -8,6 +8,7 @@
 #ifndef PWMGENERATOR_H_
 #define PWMGENERATOR_H_
 
+#include <stdint.h>
 #include "../HardwareInit/IHardware.h"
 
 class PwmGenerator : public IHardware{
@@ -15,15 +16,32 @@ public:
 	PwmGenerator();
 	virtual ~PwmGenerator();
 
-	bool IHardware::hardwareDeinit();
-	bool IHardware::hardwareInit();
+	bool hardwareDeinit();
+	bool hardwareInit();
+
+	void setPwmPeriod(uint32_t period);
+	uint32_t getPwmPeriod();
+
+	void setPWMFreq(uint32_t freq);
+	uint32_t getPwmFreq();
+
+	void setPwmDeadtime(uint32_t dt);
+	uint32_t getPwmDeadtime();
+
+	void setPwmCompare(uint32_t compare);
+	uint32_t getPwmCompare();
+
 
 protected:
 
-	bool IHardware::prerequest();
-	bool IHardware::postOperations();
+	bool prerequest();
+	bool postOperations();
 private:
 
+	uint32_t period_reg;
+	uint32_t freq;
+	uint32_t deadtime_reg;
+	uint32_t compare_reg;
 };
 
 #endif /* PWMGENERATOR_H_ */
